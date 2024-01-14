410990748a35f56023cd6a0fd5c6b948aae532c7
Sapehiasahil1 <officialsahilsapehia03@gmail.com>
2024-01-14 13:23:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tweetsy/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.tweetsy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.tweetsy.screens.CategoryScreen
+import com.example.tweetsy.screens.DetailScreen
+import com.example.tweetsy.ui.theme.TweetsyTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TweetsyTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(text = "Tweetsy")
+                            },
+                            modifier = Modifier
+                                .background(Color.Black)
+                        )
+                    }
+                ) {
+                    Box(modifier = Modifier.padding(it)) {
+                        App()
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App() {
+
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "category") {
+        //composable are used to define nodes in the NavGraph
+        composable(route = "category") {
+            CategoryScreen {
+                navController.navigate("detail/${it}")
+            }
+        }
+        composable(route = "detail/{category}",
+            arguments = listOf(
+                navArgument("category") {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            DetailScreen()
+        }
+    }
+
+}
